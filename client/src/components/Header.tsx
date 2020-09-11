66126102ceeae172ba6354f9a9203cb2cbaad3ea
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-09-11 14:25:40 +0900

    ヘッダーをデザイン

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBars,
+  faCalendar,
+  faHashtag,
+  faHome,
+  faSignInAlt
+} from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <div className="header">
-      <Link to="/">みんなのレシピ</Link>
-      {' | '}
-      <Link to="/tags">見つける</Link>
-      {' | '}
-      ログイン
+      <div className="header-bar">
+        <div className="header-title">
+          {"タイトル"}
+        </div>
+        <div className="header-button">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+      </div>
+      <Link to="/" className="header-item">
+        <div className="header-item-icon">
+          <FontAwesomeIcon icon={faHome} />
+        </div>
+        <div className="header-item-caption">
+          みんなのレシピ
+        </div>
+      </Link>
+      <Link to="/tags" className="header-item">
+        <div className="header-item-icon">
+          <FontAwesomeIcon icon={faHashtag} />
+        </div>
+        <div className="header-item-caption">
+          見つける
+        </div>
+      </Link>
+      <Link to="/" className="header-item">
+        <div className="header-item-icon">
+          <FontAwesomeIcon icon={faCalendar} />
+        </div>
+        <div className="header-item-caption">
+          記録を見る
+        </div>
+      </Link>
+      <Link to="/" className="header-item">
+        <div className="header-item-icon">
+          <FontAwesomeIcon icon={faSignInAlt} />
+        </div>
+        <div className="header-item-caption">
+          ログイン
+        </div>
+      </Link>
     </div>
   );
 }
